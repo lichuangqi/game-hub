8b12b3143bd0b50117edcb5d291752400b4ea1db
Charlie <charlie.chuangqi.li@gmail.com>
2023-12-06 22:55:53 +1000

    refractor genre and platforms

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,22 @@
-import useData from "./useData";
-
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+import platforms from "../data/platforms";
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatform = () => useData<Platform>("/platforms/lists/parents");
+const usePlatform = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24H
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatform;
